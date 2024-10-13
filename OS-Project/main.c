77d4312c4032812826c6ad41b299e2b8616795a6
Ahmet Gürkan Gönül <125986265+ahmetgurkangonul@users.noreply.github.com>
2024-10-13 18:43:50 +0300

    Update main.c

--- OS-Project/main.c
+++ OS-Project/main.c
@@ -6,9 +6,67 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+#define GRID_SIZE 4
+#define SHIP_COUNT 4
+
+void createGrid(int grid[GRID_SIZE][GRID_SIZE]) {
+    
+    for (int i = 0; i < GRID_SIZE; i++) {
+        for (int j = 0; j < GRID_SIZE; j++) {
+            grid[i][j] = 0;
+        }
+    }
+}
+
+void placeShips(int grid[GRID_SIZE][GRID_SIZE]) {
+    
+    int placed = 0;
+    
+    while (placed < SHIP_COUNT) {
+        int x = rand() % GRID_SIZE;
+        int y = rand() % GRID_SIZE;
+        if (grid[x][y] == 0) {
+            grid[x][y] = 1;
+            placed++;
+        }
+    }
+}
+
+void printGridStatus(int grid[GRID_SIZE][GRID_SIZE]) {
+    
+    printf("Grid:\n");
+    
+    for (int i = 0; i < GRID_SIZE; i++) {
+        for (int j = 0; j < GRID_SIZE; j++) {
+            printf("%d ", grid[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void makeRandomShot(int *x, int *y) {
+    
+    *x = rand() % GRID_SIZE;
+    *y = rand() % GRID_SIZE;
+}
+
+int allShipsDestroyed(int grid[GRID_SIZE][GRID_SIZE]) {
+    
+    for (int i = 0; i < GRID_SIZE; i++) {
+        for (int j = 0; j < GRID_SIZE; j++) {
+            if (grid[i][j] == 1) {
+                return 0;
+            }
+        }
+    }
+    
+    return 1;
+}
+
+int main(void) {
+
     return 0;
 }
+
