b2737532de78e5dd7bdaa993ed1c294ffdc882b2
Ahmet Gürkan Gönül <125986265+ahmetgurkangonul@users.noreply.github.com>
2024-10-13 18:42:53 +0300

    Initial Commit

---
+++ OS-Project/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  OS-Project
+//
+//  Created by Ahmet Gürkan Gönül on 13.10.2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
